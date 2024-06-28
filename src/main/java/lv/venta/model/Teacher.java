3e7bff17556638f2a62742b66dced8c2336a96cb
KOT-MEOW <QAZRFV123a>
2024-06-29 01:45:03 +0300

    Teacher model without connection

--- src/main/java/lv/venta/model/Teacher.java
+++ src/main/java/lv/venta/model/Teacher.java
@@ -1,5 +1,43 @@
 package lv.venta.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "Teacher_table")
+@Entity
 public class Teacher {
 
+	@Id
+	@Column(name = "Idt")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private int id;
+	
+	@Column(name = "Name")
+	@NotNull
+	private String name;
+	
+	@Column(name = "Surname")
+	@NotNull
+	private String surname;
+	
+	public Teacher(String name, String surname) {
+		setName(name);
+		setSurname(surname);
+	}
+	
 }

83c55710bf172c2443fef46af08df23c287e47b5
KOT-MEOW <QAZRFV123a>
2024-06-29 03:19:20 +0300

    Repos created

---
+++ src/main/java/lv/venta/repo/IChildRatingRepo.java
@@ -0,0 +1,9 @@
+package lv.venta.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.venta.model.ChildRating;
+
+public interface IChildRatingRepo extends CrudRepository<ChildRating, Integer> {
+
+}
---
+++ src/main/java/lv/venta/repo/IChildrenGroupRepo.java
@@ -0,0 +1,9 @@
+package lv.venta.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.venta.model.ChildrenGroup;
+
+public interface IChildrenGroupRepo extends CrudRepository<ChildrenGroup, Integer> {
+
+}
